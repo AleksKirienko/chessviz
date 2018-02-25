2f507a142235ffe796f792dd1df1749b03cfbb4b
Aleksandra Kirienko <aleks.Kirienko@bk.ru>
2018-02-25 22:43:29 +0700

    View of chessboard

--- chess.c
+++ chess.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void Deck(char deck[8][8]) 
+{
+	char k[]={'r','n', 'b', 'q', 'k', 'b', 'n', 'r'};
+	for (int s = 0; s < 8; ++s) 
+	{
+		deck[0][s] = k[s];
+		deck[7][s] = k[s]-32;
+		deck[1][s] = 'p';
+		deck[6][s] = 'p'-32;
+		for (int i = 2; i < 6; ++i) 
+		{
+			deck[i][s] = ' ';
+		}
+	}
+}
+
 void printDeck(char deck[8][8]) 
 {
 	for (int n = 0; n < 8; ++n)
@@ -16,5 +32,8 @@ void printDeck(char deck[8][8])
 int main() 
 {
 	char deck[8][8];
+	Deck(deck);
 	printDeck(deck);
 }
+
+
